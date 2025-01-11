f27e719c20a671be90314036b14ef7aa2cb9adf0
Seongbeom Park <seongbeom_park@tmax.co.kr>
2025-01-11 22:39:16 +0900

    Update 20250111_1295.c

--- code/20250111_1295.c
+++ code/20250111_1295.c
@@ -7,7 +7,7 @@ int main() {
 	char str[2000];
 	scanf("%s", str);
 	int i;
-	for (i=0; str[i] != '\0'; i++) {
+	for (i=0; str[i] != '\0'; i++) { // for문을 이용한 문자열 iteration
 		if (str[i] >= 'a' && str[i] <= 'z') str[i] += 'A' - 'a';
 		else if (str[i] >= 'A' && str[i] <= 'Z') str[i] += 'a' - 'A';
 	}
