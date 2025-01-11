3d22a2191c4f8664eee1449b9e006eaada69806e
Seongbeom Park <seongbeom_park@tmax.co.kr>
2025-01-11 19:38:53 +0900

    Create 20250111_1295.c

---
+++ code/20250111_1295.c
@@ -0,0 +1,16 @@
+// 1295 : 알파벳 대소문자 변환
+// https://codeup.kr/problem.php?id=1295
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	char str[2000];
+	scanf("%s", str);
+	int i;
+	for (i=0; str[i] != '\0'; i++) {
+		if (str[i] >= 'a' && str[i] <= 'z') str[i] += 'A' - 'a';
+		else if (str[i] >= 'A' && str[i] <= 'Z') str[i] += 'a' - 'A';
+	}
+	printf("%s", str);
+	return 0;
+}
