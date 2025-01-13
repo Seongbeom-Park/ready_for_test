aa4d77ab29fb1b5e0d27a9cd8ab69cd01d69a2a0
Seongbeom Park <seongbeom_park@tmax.co.kr>
2025-01-14 00:50:34 +0900

    Create problem1.c

---
+++ code/problem1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	printf("%d", n * (n-1) / 2);
+	return 0;
+}
+
+/*
+
+n	answer
+0	0
+1	0
+2	1
+3	3
+4	6
+5	10
+n	n * (n-1) / 2
+
+*/
